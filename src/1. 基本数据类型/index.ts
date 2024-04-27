624606443e8bdde454c4028c6b6770ad7a950530
littleMeow0 <142789815+littleMeow0@users.noreply.github.com>
2024-04-27 14:16:59 +0800

    类型层级比对

--- src/1. 基本数据类型/index.ts
+++ src/1. 基本数据类型/index.ts
@@ -1,10 +1,12 @@
-/**
- * 1. 给变量规定了类型，那么之后赋予变量的值类型一定要遵循这个标准，不然ts就会报错
- * 2. void 类型其实是 undefined | null 的交叉类型 （一般用于函数中，标识函数没有返回值）
- * 3. object 和 function 其实并不属于基础数据类型，因为直接写的话并不能给它约束（根据场景选择）
- */
-
 namespace BaseType {
+
+    /**
+     * 1. 给变量规定了类型，那么之后赋予变量的值类型一定要遵循这个标准，不然ts就会报错
+     * 2. void 类型其实是 undefined | null 的交叉类型 （一般用于函数中，标识函数没有返回值）
+     * 3. object 和 function 其实并不属于基础数据类型，因为直接写的话并不能给它约束（根据场景选择）
+     */
+
+
     let a:number = 1
     console.log(a)
 
@@ -32,6 +34,8 @@ namespace BaseType {
     function fn():void {
         console.log('我是一个函数')
     }
+
+    
 }
 
 
---
+++ src/2. 任意数据类型(类型层级)/index.ts
@@ -0,0 +1,92 @@
+namespace AnyType {
+
+    /**
+     * 1. any,unknown 属于顶级类型，使用它们做变量类型可以为变量的值赋任何类型的值（关掉限制）
+     * 2. Object 属于第二级别，js中我们使用的所有数据类型和函数本质上来说都是一个Object，Object包含所有的数据类型了
+     * 3. Number String Boolean 可以使用日常的方式进行直接赋值，也可以用new Number()
+     * 4. number string boolean 可以使用日常的方式进行直接赋值
+     * 5. 做类型声明，可以直接使用值   例如：1 ‘2’ false
+     * 6. never类型，最底层的数据类型，一般用于容错
+     */
+
+
+    let a:any = '123'
+    a = []
+    a = null
+    a = function ():void {
+
+    }
+    a = new Date()
+    a = {
+        name: '小韩a'
+    }
+    console.log(a)
+    console.log(a.name)   // any 类型的变量属性可被读取
+
+    let a1: number = 11
+    a1 = a    // any 类型的变量可以赋值给其它类型的变量（被赋值的这个变量类型由此不确定了）
+    console.log(a1)
+
+
+    let b:unknown = '123'
+    b = []
+    b = null
+    b = function ():void {
+
+    }
+    b = new Date()
+    b = {
+        name: '小韩b'
+    }
+    console.log(b)
+    // console.log(b.name)   // unknown 类型的变量属性不可被读取
+
+    let b1:number = 22
+    // b1 = b    // 可以发现这行代码是错误的，unknown 类型的变量不可被赋值给其它变量 （由此发现，unknown 要比 any 更安全）
+
+    let b2:unknown = 0
+    b2 = b  // unknown 类型的变量可被赋值给同类型的变量
+
+
+    let c:Object = {
+        name: 'c'
+    }
+    c = 123
+    c = '123'
+    c = function ():void {
+
+    }
+    c = new Date()
+    c = null
+    c = undefined
+    c = new Object({
+        name: 'c'
+    })
+    console.log(c)
+
+
+    // c = b
+
+
+
+
+    let d:Number = 10
+    d = new Number(11)
+    console.log(d)
+
+
+    let e:number = 10
+    // e = new Number(20)
+    console.log(e)
+
+
+    type f = 1 | '1' | false
+    let g:f = 1
+    // g = 2
+    console.log(g)
+
+
+
+}
+
+
