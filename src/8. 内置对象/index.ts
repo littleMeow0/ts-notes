31c2f2746f836c61b1c8688ca6a755e5c93c085f
littleMeow0 <142789815+littleMeow0@users.noreply.github.com>
2024-04-30 17:38:13 +0800

    添加内置对象

---
+++ src/8. 内置对象/index.ts
@@ -0,0 +1,12 @@
+namespace InnerObject{
+
+    /**
+     * 内置对象   
+     * ES5 ===>  Boolean Number String Object Date RegExp Error XMLHttpRequest
+     * DOM ===>  HTMLElement NodeList(元素集合) NodeListOf<HTMLElement>(可以在元素集合中加类型)
+     * BOM ===>  Storage Location  Promise<number>  
+     */
+    
+    
+
+}
